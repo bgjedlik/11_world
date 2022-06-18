f83c72a338e179395a290a7ed93e10f747758c96
Bólya Gábor <bolya.gabor@gmail.com>
2022-06-18 09:39:33 +0200

    country data

---
+++ src/components/CountryTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="table">
+    <thead>
+        <tr>
+            <th>Country name</th>
+            <th>Code</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="country in countries" :key="country.id">
+            <td>{{ country.name }}</td>
+            <td>{{ country.iso3}}</td>
+        </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    name:'country-table',
+    props:['countries']
+}
+</script>
+
+<style>
+
+</style>
